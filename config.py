a7956361ed7769c69b13ad77a08b0739cf6682cf
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2026-02-08 17:07:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,11 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7630552476:AAGdSH7FUl3IqwzTbLQIuaRC3eMv46Qre-A")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7773537686:AAFJ6T3vzvS-7wnIeBs0-bwfvwmhcHg2oTA")
     API_ID = int(os.environ["API_ID",
-"25513009"]
+"22470912"]
     API_HASH = os.environ["API_HASH",
-"33ace9b0779df4387a8ad8b7fbe6b050"]
-    AUTH_USERS = ""7068035738""
+"511be78079ed5d4bd4c967bc7b5ee023"]
+    AUTH_USERS = ""7678862761""
  
+
